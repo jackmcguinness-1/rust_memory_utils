9f527d5490af06fad58ac8641055fe4fce9c6264
jackmcguinness-1 <44623256+jackmcguinness-1@users.noreply.github.com>
2022-08-07 21:24:39 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+mod util;
+
+#[cfg(test)]
+mod tests {
+    use crate::util::flattened_array::FlatByteMatrix32;
+
+    #[test]
+    fn it_works() {
+        
+        let mut mat = FlatByteMatrix32::new(4, 4);
+
+        for i in 0..4{
+            for j in 0..4{
+                mat[i][j] = (i + 4*j) as u8;
+            }
+        }
+
+        for i in 0..4{
+            println!("{:?}", mat[i].iter());
+        }
+
+        println!("{:?}", mat.col_major_data);
+
+    }
+}
---
+++ src/util/mod.rs
@@ -0,0 +1,1 @@
+pub mod flattened_array;
